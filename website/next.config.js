bc446744d322eaa48b42c5bb824605683614166b
Chardash <nebojsa.baabic@gmail.com>
2022-11-08 19:08:08 +0100

    Expose env vars to nextjs runtime

--- website/next.config.js
+++ website/next.config.js
@@ -3,4 +3,8 @@ module.exports = {
         locales: ['en', 'sr'],
         defaultLocale: 'en',
     },
+    env: {
+        MUSEUM_APP_ENDPOINT: process.env.MUSEUM_APP_ENDPOINT,
+        KEYCLOAK_ENDPOINT: process.env.KEYCLOAK_ENDPOINT
+    }
 }
